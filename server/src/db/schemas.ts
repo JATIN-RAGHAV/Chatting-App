f9f6964508c671d7c1ec0a6762afa00d4e43fba5
JATIN-RAGHAV <jrag137@gmail.com>
2024-05-06 21:50:09 +0530

    Added chatting routes to send and get the messages have not tested the routes yet.

--- server/src/db/schemas.ts
+++ server/src/db/schemas.ts
@@ -6,6 +6,19 @@ const userSchema = new mongoose.Schema({
     friends:[{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     inFriendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     outFriendRequests: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+    chat:[
+            {
+                chatId:{type: mongoose.Schema.Types.ObjectId, ref: 'Chat'},
+                with: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+            }]
 });
 
+const chatSchema = new mongoose.Schema({
+    chat:[{
+        sender: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+        message: String
+    }]
+})
+
+export const Chat = mongoose.model('Chat', chatSchema);
 export const User = mongoose.model('User', userSchema);
--- server/src/routes/userRoutes/chattingRoutes.ts
+++ server/src/routes/userRoutes/chattingRoutes.ts
@@ -1,10 +1,52 @@
 import express  from "express";
-import { User } from "../../db/schemas";
+import { User, Chat } from "../../db/schemas";
 
 const Router = express.Router()
 
-Router.post('/', async (req, res) => {
+Router.post('/send', async (req, res) => {
+    // Expected body type {receiver: receiverUsername, message: message}
+    const receiver: String = req.body.receiver;
+    const message: String = req.body.message;
+    const user = await User.findOne({username: req.body.serverData.user.username});
+    if(user){
+        const receiverObject = await User.findOne({username: receiver})
+        if(receiverObject){
+            if(user.friends.some(id => id.equals(receiverObject._id))){
+                if(!user.chat.some(chats => chats.with?.equals(receiverObject._id))){
+                    const newChatObjectContent = new Chat({chat:[{sender:user._id, message}]})
+                    await newChatObjectContent.save()
+                    user.chat.push({chatId: newChatObjectContent._id, with:receiverObject.username})
+                    receiverObject.chat.push({chatId: newChatObjectContent._id, with: user.username})
+                }else{
+                    const chatId = user.chat.find(chat => chat.with?.equals(receiverObject._id))
+                    const chatObject = await Chat.findById(chatId)
+                    if(chatObject){
+                        chatObject.chat.push({sender: user._id, message})
+                        await chatObject.save()
+                        res.json({message: 'Message sent successfully'})
+                    }
+                }
+            }else res.status(409).json({message: 'Your are not friends with the other person'})
+        }else res.status(404).json({message: 'Receiver not found'})
+    }else res.status(404).json({message: 'User not found'})
+})
 
+Router.get('/get', async (req, res) => {
+    // Expects a body {with: username of other person}
+    const otherPerson:String = req.body.with;
+    const user = await User.findOne({username: req.body.serverData.user.username});
+    if(user){
+        const other = await User.findOne({username: otherPerson});
+        if(other){
+            const chatId = user.chat.find(chat => chat.with?.equals(other._id))
+            if(chatId){
+                const chatObject = await Chat.findById(chatId)
+                if(chatObject){
+                    res.json({chat: chatObject.chat})
+                }else res.status(404).json({message: 'Chat not found'})
+            }
+        }
+    }else res.status(404).json({message: 'User not found'})
 })
 
 export default Router
