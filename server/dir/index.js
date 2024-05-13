0dda7cffe3af74970d564c48a0b6392ed3549078
JATIN-RAGHAV <jrag137@gmail.com>
2024-05-13 17:38:17 +0530

    made chages to js

--- server/dir/index.js
+++ server/dir/index.js
@@ -9,7 +9,17 @@ const cors_1 = __importDefault(require("cors"));
 const route_1 = __importDefault(require("./routes/route"));
 const config_1 = require("./config");
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)());
+// Configure CORS to allow requests from any origin
+const corsOptions = {
+    origin: '*', // Allow requests from any origin
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+app.use((req, res, next) => {
+    console.log(req.body);
+    console.log(req.headers);
+    next();
+});
+app.use((0, cors_1.default)(corsOptions));
 app.use(express_1.default.json());
 app.use(route_1.default);
 mongoose_1.default.connect(config_1.mongodbUrl);
